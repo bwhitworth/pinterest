8372cd6c9ac0a2d409ce0cfa6ffafbb507146b32
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-03-30 16:59:14 -0500

    changed some style, added code comments, and re-organized

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,11 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import boardsComp from '../../components/boards/boards';
 
+// ASSIGN DIV ID'S TO VARIABLES FOR ADDING AND REMOVING CLASSES
 const authDiv = $('#auth');
 const homeDiv = $('#home');
 const boardsDiv = $('#boards');
-const logoutButton = $('#navbar-logout-btn');
+const logoutButton = $('#logout-btn');
 
+// ADD OR REMOVE THE 'HIDE' CLASS OF DIVS, BASED ON USER LOGGED IN OR OUT
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
@@ -13,6 +16,7 @@ const checkLoginStatus = () => {
       homeDiv.addClass('hide');
       boardsDiv.removeClass('hide');
       logoutButton.removeClass('hide');
+      boardsComp.boardBuilder();
     } else {
       authDiv.removeClass('hide');
       homeDiv.removeClass('hide');
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,18 +2,14 @@ import 'bootstrap';
 import '../styles/main.scss';
 import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
-import auth from './components/auth/auth';
+import auth from './components/home/home';
 import authData from './helpers/data/authData';
-import boardsComp from './components/boards/boards';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   auth.loginButton();
   authData.checkLoginStatus();
   auth.logoutEvent();
-  firebase.auth().onAuthStateChanged(() => {
-    boardsComp.boardBuilder();
-  });
 };
 
 init();
