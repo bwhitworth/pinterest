c82791bb343f19f99d39c7bbd3abbd75680ee96c
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-04-11 14:01:45 -0500

    changed dropdown to radio buttons

--- src/javascripts/components/pins/pinEditor.js
+++ src/javascripts/components/pins/pinEditor.js
@@ -8,21 +8,18 @@ const showPinEditor = (e) => {
   const pinId = e.target.closest('.pin-card').id;
   let domString = '';
   domString += `<h2 id=${pinId}>Edit Pin</h2>`;
-  domString += '<div class="dropdown">';
-  domString += '  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Select Board:</button>';
+  domString += '<div class="custom-control custom-radio col">';
   const currentUserUid = firebase.auth().currentUser.uid;
   boardData.getUserBoards(currentUserUid)
     .then((boards) => {
       boards.forEach((b) => {
-        domString += '  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">';
-        domString += `<a class="dropdown-item" id="${b.id}">${b.name}</a`;
+        domString += `<input type="radio" id="${b.id}" name="customRadio" class="custom-control-input board-radio-btns">`;
+        domString += `<label class="custom-control-label pr-2" for="${b.name}">${b.name}</label><br>`;
       });
-      domString += '  </div></div>';
+      domString += '  </div>';
       utils.printToDom('single-container', domString);
     })
     .catch((err) => console.error('problem with boardBuilder', err));
-
-  utils.printToDom('single-container', domString);
   console.error(pinId);
 };
 
